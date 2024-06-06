54297a9996ea7bf1e294c4aabe2dc9fb00022687
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-06-05 23:27:41 -0300

    Finalizing the creation of dynamic views for each movie

--- filme/urls.py
+++ filme/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import Homepage, Homefilmes, DetalhesFilme
 
+app_name = 'filme'
+
 urlpatterns = [
-    path('', Homepage.as_view()),
-    path('filmes/', Homefilmes.as_view()),
-    path('filmes/<int:pk>', DetalhesFilme.as_view()), #<int pk> -> primary key inteira do modelo passada nas views
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', DetalhesFilme.as_view(), name='detalhesfilme'), #<int pk> -> primary key inteira do modelo passada nas views
 ]
