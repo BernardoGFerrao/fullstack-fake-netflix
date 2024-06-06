4bf102f70e03ade2f2a62926877c040ebad85052
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-06-06 20:44:14 -0300

    Dynamic edits with JavaScript and Featured Movie

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -7,3 +7,7 @@ def lista_filmes_recentes(request):
 def lista_filmes_alta(request):
     lista_filmes = Filme.objects.all().order_by('-visualizacoes')[0:8]#- Decrescente
     return {"lista_filmes_alta": lista_filmes}
+
+def filme_destaque(request):
+    filme = Filme.objects.order_by('-data_de_criacao')[0]#- Decrescente
+    return {"filme_destaque": filme}
