124b3f79ae7d24e92fe7488829ce0474e7b1cc05
Bernardo Ferrão <143981716+BernardoGFerrao@users.noreply.github.com>
2024-06-14 11:38:55 -0300

    Update settings.py

--- netflix/settings.py
+++ netflix/settings.py
@@ -94,11 +94,11 @@ DATABASES = {
 
 import dj_database_url
 
-DATABASE_URL = 'os.getenv("DATABASE_URL")'
+DATABASE_URL = 'postgresql://postgres:vmfOLXGqEAAzyYcGJhAxAbOgtpHpnNwn@viaduct.proxy.rlwy.net:38227/railway'
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.config(default='postgresql://postgres:vmfOLXGqEAAzyYcGJhAxAbOgtpHpnNwn@viaduct.proxy.rlwy.net:38227/railway', conn_max_age=1800),
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
         }
 
 
